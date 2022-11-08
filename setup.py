d95504fe887c16cd88fda9bb589bfa5b76bc4bc7
cordis <cordis@game-mafia.ru>
2022-11-08 14:59:55 +0700

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setuptools.setup(
     name='rmshared',
-    version='0.2.1',
+    version='0.2.2',
     author='CordiS',
     author_email='cordis@rebelmouse.com',
     url='https://github.com/RebelMouseTeam/rmshared',
     license='Apache 2.0',
-    packages=['rmshared'],
+    packages=['rmshared', 'rmshared.content', 'rmshared.content.taxonomy'],
     test_suite='tests',
     classifiers=[
         'Intended Audience :: Developers',
