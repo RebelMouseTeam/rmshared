82ce9cddefe906fe6fc066f25bf6219d6d6dfea1
cordis <cordis@game-mafia.ru>
2022-11-08 15:26:19 +0700

    Bump version: 0.3.2 → 0.3.3

--- rmshared/requests/__init__.py
+++ rmshared/requests/__init__.py
@@ -1,4 +1,4 @@
-from rebelmouse.requests.factory import Factory
-from rebelmouse.requests.interfaces import IRequest
+from rmshared.requests.factory import Factory
+from rmshared.requests.interfaces import IRequest
 
 __all__ = ['Factory', 'IRequest']
