1ba896b7638dbdf9eb6968e1712a4da84c86c2aa
cordis <cordis@game-mafia.ru>
2023-03-16 17:25:25 +0700

    + exposing operators for testing [TX-114]

--- rmshared/content/taxonomy/core/variables/__init__.py
+++ rmshared/content/taxonomy/core/variables/__init__.py
@@ -1,15 +1,16 @@
 from rmshared.content.taxonomy.core.variables import arguments
+from rmshared.content.taxonomy.core.variables import operators
 from rmshared.content.taxonomy.core.variables import protocol
 from rmshared.content.taxonomy.core.variables.abc import Argument
+from rmshared.content.taxonomy.core.variables.abc import Operator
 from rmshared.content.taxonomy.core.variables.abc import Reference
 from rmshared.content.taxonomy.core.variables.abc import IResolver
 from rmshared.content.taxonomy.core.variables.abc import IProtocol
 from rmshared.content.taxonomy.core.variables.resolver import Resolver
 
 __all__ = (
-    'arguments',
-
-    'Argument',
+    'Argument', 'arguments',
+    'Operator', 'operators',
     'Reference',
 
     'IResolver', 'Resolver',
