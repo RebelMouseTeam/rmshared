91c651ac91ef52b942d03ad100cc0f5c16340375
cordis <cordis@game-mafia.ru>
2023-04-28 16:33:49 +0200

    Bump version: 0.4.52 → 0.4.53

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='rmshared',
-    version='0.4.52',
+    version='0.4.53',
     author='CordiS',
     author_email='cordis@rebelmouse.com',
     url='https://github.com/RebelMouseTeam/rmshared',
