52bff4ef49e5e3129cf448ffd86b83340c1aa962
cordis <cordis@game-mafia.ru>
2023-06-05 11:46:07 +0700

    + upgrading read-only dict to being pickleable [TX-114]

--- rmshared/typings.py
+++ rmshared/typings.py
@@ -1,3 +1,5 @@
+from copy import copy
+from copy import deepcopy
 from functools import cached_property
 from typing import Union, Type, Callable, TypeVar, Any
 
@@ -28,13 +30,17 @@ def read_only(value):
         raise NotImplementedError(type(value))
 
 
-class ReadOnlyDict(dict):
+class ReadOnlyDict(dict):  # TODO: consider https://pypi.org/project/frozendict/
     """
     @see: https://stackoverflow.com/questions/19022868/how-to-make-dictionary-read-only-in-python/31049908
     @see: https://stackoverflow.com/questions/1151658/python-hashable-dicts
     """
+
+    def __repr__(self):
+        return f'{type(self).__name__}({sorted(self._sorted_items)}'
+
     def __hash__(self):
-        return hash(tuple(self._sorted_items))
+        return hash(tuple(self._sorted_items))  # TODO: Consider `frozenset()` instead of `tuple(sorted())`
 
     @cached_property
     def _sorted_items(self):  # TODO: Seems safe but worth checking in the process
@@ -42,6 +48,15 @@ class ReadOnlyDict(dict):
             return sorted(self.items())
         except TypeError:
             return sorted(map(str, self.items()))
+
+    def __copy__(self):
+        return type(self)(copy(list(self.items())))
+
+    def __deepcopy__(self, memo):
+        return type(self)(deepcopy(list(self.items()), memo))
+
+    def __reduce__(self):
+        return type(self), (list(self.items()), )
 
     def __readonly__(self, *args, **kwargs):
         raise RuntimeError("Cannot modify ReadOnlyDict")
