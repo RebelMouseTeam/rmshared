eaf49a06d91ca7be1a132c7210ce58fb9e4fcec4
cordis <cordis@game-mafia.ru>
2023-07-26 14:05:04 +0800

    + adding `total_ordering` for labels [TX-151]

--- rmshared/content/taxonomy/core/labels.py
+++ rmshared/content/taxonomy/core/labels.py
@@ -3,25 +3,31 @@ from dataclasses import dataclass
 from typing import Generic
 from typing import TypeVar
 
+from rmshared.dataclasses import total_ordering
+
 Field = TypeVar('Field')
 Value = TypeVar('Value')
 
 
 @dataclass(frozen=True)
+@total_ordering
 class Label(Generic[Field], metaclass=ABCMeta):
     field: Field
 
 
 @dataclass(frozen=True)
+@total_ordering
 class Value(Label[Field], Generic[Field, Value]):
     value: Value
 
 
 @dataclass(frozen=True)
+@total_ordering
 class Badge(Label[Field]):
     pass
 
 
 @dataclass(frozen=True)
+@total_ordering
 class Empty(Label[Field]):
     pass
