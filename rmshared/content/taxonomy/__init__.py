c70d5f97bea961af27da1ff521932ea4b5cecc79
cordis <cordis@game-mafia.ru>
2024-04-28 14:07:30 +0700

    + exposing sections [DD-805]

--- rmshared/content/taxonomy/__init__.py
+++ rmshared/content/taxonomy/__init__.py
@@ -2,21 +2,25 @@ from rmshared.content.taxonomy import core
 from rmshared.content.taxonomy import posts
 from rmshared.content.taxonomy import users
 from rmshared.content.taxonomy import graph
+from rmshared.content.taxonomy import sections
 from rmshared.content.taxonomy import variables
 from rmshared.content.taxonomy import extractors
 from rmshared.content.taxonomy.abc import Guid
 from rmshared.content.taxonomy.fakes import Fakes
 
-__all__ = [
+__all__ = (
     'Guid',
 
     'core',
+
     'posts',
+    'sections',
     'users',
+
     'graph',
 
     'variables',
     'extractors',
 
     'Fakes',
-]
+)
