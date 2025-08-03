125e385e6a1c9b516264c1e0b5fc7e7f12cdbfe7
cordis <cordis@game-mafia.ru>
2025-08-03 12:56:03 +0700

    f fixing imports [TX-216]

--- rmshared/content/taxonomy/variables/__init__.py
+++ rmshared/content/taxonomy/variables/__init__.py
@@ -1,15 +1,3 @@
-from rmshared.content.taxonomy.variables import values
-from rmshared.content.taxonomy.variables import arguments
-from rmshared.content.taxonomy.variables import operators
-from rmshared.content.taxonomy.variables import protocols
-from rmshared.content.taxonomy.variables.abc import Argument
-from rmshared.content.taxonomy.variables.abc import IResolver
-from rmshared.content.taxonomy.variables.abc import Operator
-from rmshared.content.taxonomy.variables.abc import Reference
-from rmshared.content.taxonomy.variables.resolver import Resolver
-from rmshared.content.taxonomy.variables.fakes import Fakes
-
-
 __all__ = (
     'values',
 
@@ -20,6 +8,19 @@ __all__ = (
     'IResolver', 'Resolver',
 
     'protocols',
+    'traversal',
 
     'Fakes',
 )
+
+from rmshared.content.taxonomy.variables import values
+from rmshared.content.taxonomy.variables import arguments
+from rmshared.content.taxonomy.variables import operators
+from rmshared.content.taxonomy.variables import protocols
+from rmshared.content.taxonomy.variables import traversal
+from rmshared.content.taxonomy.variables.abc import Argument
+from rmshared.content.taxonomy.variables.abc import IResolver
+from rmshared.content.taxonomy.variables.abc import Operator
+from rmshared.content.taxonomy.variables.abc import Reference
+from rmshared.content.taxonomy.variables.resolver import Resolver
+from rmshared.content.taxonomy.variables.fakes import Fakes
