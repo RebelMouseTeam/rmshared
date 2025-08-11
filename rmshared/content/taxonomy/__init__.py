8f84090a5ba2902a8256510468f264c5c3eabc2f
cordis <cordis@game-mafia.ru>
2025-08-11 12:08:34 +0700

    f fixing missing import [TX-216]

--- rmshared/content/taxonomy/__init__.py
+++ rmshared/content/taxonomy/__init__.py
@@ -5,6 +5,7 @@ __all__ = (
     'variables',
 
     'graph',
+    'extractors',
 
     'posts',
     'users',
@@ -12,19 +13,24 @@ __all__ = (
     'sections',
     'communities',
 
-    'extractors',
+    'sql',
 
     'Fakes',
 )
 
 from rmshared.content.taxonomy import core
 from rmshared.content.taxonomy import variables
+
 from rmshared.content.taxonomy import graph
+from rmshared.content.taxonomy import extractors
+
 from rmshared.content.taxonomy import posts
 from rmshared.content.taxonomy import users
 from rmshared.content.taxonomy import tags
 from rmshared.content.taxonomy import sections
 from rmshared.content.taxonomy import communities
-from rmshared.content.taxonomy import extractors
+
+from rmshared.content.taxonomy import sql
+
 from rmshared.content.taxonomy.abc import Guid
 from rmshared.content.taxonomy.fakes import Fakes
